f84ae23ee0258ab428e8940c04998df39549c6c1
Núria Ramoneda <nuriarai@gmail.com>
2022-10-04 20:04:43 +0200

    Add functionality

--- public/js/strictEquals/strictEquals.js
+++ public/js/strictEquals/strictEquals.js
@@ -1,5 +1,22 @@
-const strictEquals = () => {
-  console.log("hola");
+const strictEquals = (number1, number2) => {
+  let result;
+  if (Number.isNaN(number1) && Number.isNaN(number2)) {
+    result = false;
+    return `Result: ${number1} / ${number2} : ${result}`;
+  }
+
+  if (Object.is(0, number1) && Object.is(-0, number2)) {
+    result = true;
+    return `Result: ${number1} / ${number2} : ${result}`;
+  }
+
+  if (Object.is(-0, number1) && Object.is(0, number2)) {
+    result = true;
+    return `Result: ${number1} / ${number2} : ${result}`;
+  }
+
+  result = Object.is(number1, number2);
+  return `Result: ${number1} / ${number2} : ${result}`;
 };
 
 export default strictEquals;
