dc755ab714adeaaed5bb9a10eb71cf55be42333a
Núria Ramoneda <nuriarai@gmail.com>
2022-10-04 22:13:50 +0200

    Rename variables and outside the verbose output to the console log.

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,10 @@
 import strictEquals from "./strictEquals/strictEquals.js";
 
-console.log(strictEquals(1, 1));
-console.log(strictEquals(NaN, NaN));
-console.log(strictEquals(0, -0));
-console.log(strictEquals(-0, 0));
-console.log(strictEquals(1, "1"));
-console.log(strictEquals(true, false));
-console.log(strictEquals(false, false));
-console.log(strictEquals("Water", "oil"));
+console.log(`Result 1 / 1 => ${strictEquals(1, 1)}`);
+console.log(`Result NaN / NaN => ${strictEquals(NaN, NaN)}`);
+console.log(`Result 0 / -0 => ${strictEquals(0, -0)}`);
+console.log(`Result -0 / 0 => ${strictEquals(-0, 0)}`);
+console.log(`Result 1 / "1" => ${strictEquals(1, "1")}`);
+console.log(`Result true  / false => ${strictEquals(true, false)}`);
+console.log(`Result false / false => ${strictEquals(false, false)}`);
+console.log(`Result "Water" / "oil" => ${strictEquals("Water", "oil")}`);
